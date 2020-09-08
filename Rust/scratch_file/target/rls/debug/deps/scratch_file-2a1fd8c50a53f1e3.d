18edd4d731cc5c4042246acb665477f61c603a65
Julian Berla <jtberla@gmail.com>
2020-09-08 15:46:11 -0400

    final configuration

--- Rust/scratch_file/target/rls/debug/deps/scratch_file-2a1fd8c50a53f1e3.d
+++
@@ -1,5 +0,0 @@
-c:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\rls\debug\deps\scratch_file-2a1fd8c50a53f1e3.rmeta: src\main.rs
-
-c:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\rls\debug\deps\scratch_file-2a1fd8c50a53f1e3.d: src\main.rs
-
-src\main.rs:
--- Rust/scratch_file/target/rls/debug/deps/scratch_file-a67a45440e9e04cd.d
+++
@@ -1,5 +0,0 @@
-c:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\rls\debug\deps\scratch_file-a67a45440e9e04cd.rmeta: src\main.rs
-
-c:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\rls\debug\deps\scratch_file-a67a45440e9e04cd.d: src\main.rs
-
-src\main.rs:
