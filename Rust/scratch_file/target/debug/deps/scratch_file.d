e899e20f772130af2da3d2358fa19c714308116a
Julian Berla <jtberla@gmail.com>
2020-09-12 17:31:08 -0400

    removed scratch_file target

--- Rust/scratch_file/target/debug/deps/scratch_file.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\debug\deps\scratch_file.exe: src\main.rs
-
-C:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\debug\deps\scratch_file.d: src\main.rs
-
-src\main.rs:
--- Rust/scratch_file/target/debug/scratch_file.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\debug\scratch_file.exe: C:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\src\main.rs
