659cb4cce87fd59c33aea48d9eae3da9a4ede1b2
Julian Berla <jtberla@gmail.com>
2020-09-12 15:41:06 -0400

    committing all changes to start work on a demo project

---
+++ Rust/scratch_file/target/rls/debug/deps/scratch_file-2a1fd8c50a53f1e3.d
@@ -0,0 +1,5 @@
+c:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\rls\debug\deps\scratch_file-2a1fd8c50a53f1e3.rmeta: src\main.rs
+
+c:\Users\jtber\Desktop\projects\scratchpad\Rust\scratch_file\target\rls\debug\deps\scratch_file-2a1fd8c50a53f1e3.d: src\main.rs
+
+src\main.rs:
